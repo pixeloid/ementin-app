abd8590ac21646d6e94ab70c59586007518e88f9
Gergo Olah <gergo.olah@possible.com>
2022-02-28 00:28:48 +0100

    auth base

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:eventapp/providers/auth.dart';
+import 'package:eventapp/screens/auth_screen.dart';
 import 'package:eventapp/screens/event_listing_screen.dart';
 import 'package:eventapp/screens/event_screen.dart';
+import 'package:eventapp/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   final HttpLink httpLink =
@@ -26,18 +30,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(
-          Theme.of(context)
-              .textTheme, // If this is not set, then ThemeData.light().textTheme is used.
-        ),
-      ),
-      home: const EventListingScreen(),
-      routes: {
-        '/event': (ctx) => EventScreen(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        )
+      ],
+      child: Consumer<Auth>(
+          builder: (ctx, auth, _) => MaterialApp(
+                title: 'Flutter Demo',
+                theme: ThemeData(
+                  textTheme: GoogleFonts.poppinsTextTheme(
+                    Theme.of(context)
+                        .textTheme, // If this is not set, then ThemeData.light().textTheme is used.
+                  ),
+                ),
+                home: auth.isAuth
+                    ? EventListingScreen()
+                    : FutureBuilder(
+                        future: auth.tryAutoLogin(),
+                        builder: (ctx, authResultSnapshot) =>
+                            authResultSnapshot.connectionState ==
+                                    ConnectionState.waiting
+                                ? SplashScreen()
+                                : AuthScreen(),
+                      ),
+                routes: {
+                  '/event': (ctx) => EventScreen(),
+                },
+              )),
     );
   }
 }
