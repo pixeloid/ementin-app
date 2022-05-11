31c9a36206918d19171ac7b2fd1567724648bb90
pixeloid <olah.gergely@pixeloid.hu>
2022-05-11 12:00:37 +0200

    noti app

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Pixeloid on 2022. 05. 11..
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
