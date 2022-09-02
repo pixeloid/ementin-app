86ae6d1be6978c56c46a1f133a38d9c92de0dbcc
pixeloid <olah.gergely@pixeloid.hu>
2022-09-02 13:34:12 +0200

    performance upgrade

--- ios/OneSignalNotificationServiceExtension/NotificationService.h
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -2,7 +2,7 @@
 //  NotificationService.h
 //  OneSignalNotificationServiceExtension
 //
-//  Created by Pixeloid on 2022. 05. 11..
+//  Created by Pixeloid on 2022. 09. 02..
 //
 
 #import <UserNotifications/UserNotifications.h>
