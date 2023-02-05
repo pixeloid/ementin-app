ce9a79b0b4c75bd51129925067f948ecf05be9d1
pixeloid <olah.gergely@pixeloid.hu>
2023-02-05 20:29:21 +0100

    main prod

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:eventapp/my_app.dart';
 
+import 'app_define/app_config.dart';
+import 'environments/environement.dart';
+
 Future<void> main() async {
+  AppConfig(env: Environment.production());
+
   await myMain();
 }
