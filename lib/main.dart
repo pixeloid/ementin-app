26611e965303cdd5baa113bc33749f91f5ded382
pixeloid <olah.gergely@pixeloid.hu>
2023-02-14 13:58:02 +0100

    prod env

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'app_define/app_config.dart';
 import 'environments/environement.dart';
 
 Future<void> main() async {
-  AppConfig(env: Environment.development());
+  AppConfig(env: Environment.production());
 
   await myMain();
 }
