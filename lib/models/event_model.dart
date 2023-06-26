7426343dd2d215a13f7adea0a164cf3924a44fa1
pixeloid <olah.gergely@pixeloid.hu>
2023-06-26 20:53:06 +0200

    fix

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -1,6 +1,5 @@
 import 'package:eventapp/models/event_registration_model.dart';
 import 'package:eventapp/models/sponsor_category.dart';
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class EventModel {
