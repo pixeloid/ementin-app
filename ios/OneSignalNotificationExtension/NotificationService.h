204412d9a222e9745de706292dcbbe4dd30d0cab
pixeloid <olah.gergely@pixeloid.hu>
2023-10-03 12:25:22 +0200

    fix onesignal

---
+++ ios/OneSignalNotificationExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationExtension
+//
+//  Created by Pixeloid on 03/10/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
